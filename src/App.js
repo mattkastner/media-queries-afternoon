25e7437e3868600ea26823099f6ca3ce2b963a80
Matthew Kastner <matthewkastner@gmail.com>
2019-09-03 15:10:54 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'reset-css'
+import './App.css'
+import './Header.css'
+import './Content.css'
+
+export default class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      clicked: false
+    }
+
+  }
+
+  // animate = () => {
+  //   this.setState({
+  //     clicked: !this.state.clicked
+  //   })
+  // }
+
+
+  render() {
+    console.log(this.state.clicked)
+    return (
+      <div className='app' onScroll={this.animate}>
+        <div className='main-container'>
+          <header className='top-bar-container'>
+            <div className='top-bar'>
+              <h1 className='company-title'>Start Bootstrap</h1>
+              <nav className='nav-list-container'>
+                <ul className='nav-list'>
+                  <li>SERVICES</li>
+                  <li>PORTFOLIO</li>
+                  <li>ABOUT</li>
+                  <li>TEAM</li>
+                  <li>CONTACT</li>
+                </ul>
+              </nav>
+              <div className='mobile-menu' onClick={() => this.setState({clicked:!this.state.clicked})}><span>MENU </span> &#9776;</div>
+            </div>
+          </header>
+          {/* Slide Out Menu */}
+          <div className={
+              this.state.clicked ?
+              'menu slide'
+              :
+              'menu'
+          }>
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Contact</a>
+          </div>
+          <div className='content-container'>
+            <div className='intro-container'>
+              <div className='middle-intro-container'>
+                <div className='welcome'>Welcome To Our Studio!</div>
+                <div className='meet-you'>IT'S NICE TO MEET YOU</div>
+                <button className='tell-me-btn'>TELL ME MORE</button>
+              </div>
+            </div>
+            <div className='services-container'></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
